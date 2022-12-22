06136e2520ab5b4e5df5d9d92241ba1b280d13c4
Shakhlo <110711642+Shakhlo90@users.noreply.github.com>
2022-12-21 19:57:26 -0500

    the pink color car

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CarFeatures AUDI= new CarFeatures();
-
+        AUDI.model="A10";
+        AUDI.engine="5000000000000HP";
+        AUDI.color="pink";
+        AUDI.name="josh";
+        System.out.println("the car is ready in pink color");
 
 
     }
